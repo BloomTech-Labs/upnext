9b9e46dd4e6130746bf1d329edfd51b8001b2087
nomyard <nomyards@gmail.com>
2018-04-13 13:40:23 -0400

    updated README/file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import './App.css';
+
+import CardView from './components/CardView';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Seen My Kid?</h1>
+      <div className="App">
+          <h1> ADMIN EVENT LIST </h1>
+          <CardView />
+         
       </div>
     );
   }
 }
 
 export default App;
+
+
+// ****PERSONAL NOTES******
+
+//Title
+//Date/Time
+//Group Name
+//Schedule
+
+//what do i need to do first? get a card component to show up (DONE)
+//what next- update that card component's state in order to make it reactive (DO NOW)
+//then? get those cards to dsiplay/delete when needed (STILL NEED TO BE DONE)
+//Need to implement breadcrumbs too (semantic-ui has a good solution for this)
