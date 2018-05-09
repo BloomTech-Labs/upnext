7ad2a4af7fc8d7864f9b38bc36ec65b7a00cb257
darkness198 <petrellvereen@gmail.com>
2018-05-08 22:17:34 -0400

    clearing old structure

--- assets/src/tests/eventUtils/EventForm.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestComponent from '../../components/eventUtils/EventForm';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TestComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
