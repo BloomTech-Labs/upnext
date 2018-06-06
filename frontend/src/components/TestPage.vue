a0e2c4e64ed5e4d7b9e7b5f53bf3b9014ca04fed
Randallwhitlock <randall.whitlock@live.com>
2018-06-06 15:20:56 -0400

    added keys to testview component to remove errors

--- frontend/src/components/TestPage.vue
+++ frontend/src/components/TestPage.vue
@@ -1,48 +1,43 @@
 <template>
-<div>
-  <mask id="masking" maskUnits="objectBoundingBox"
-  maskContentUnits="objectBoundingBox">
-  <rect y="0" width="1" height="1" fill="url(#gradient)" />
-  <circle cx=".5" cy=".5" r=".4" fill="gray" />
-  <circle cx=".5" cy=".5" r=".3" fill="white" />
-  ...
-</mask>
-
-
+  <div>
+    <mask id="masking" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
+      <rect y="0" width="1" height="1" fill="url(#gradient)" />
+      <circle cx=".5" cy=".5" r=".4" fill="gray" />
+      <circle cx=".5" cy=".5" r=".3" fill="white" /> ...
+    </mask>
 
     <v-container grid-list-md>
-      <div v-for="i in 2">
-        <v-layout  class="dual" row wrap>
+      <div :key="index" v-for="(i, index) in 2">
+        <v-layout class="dual" row wrap>
           <v-flex>
             <v-card>
               <v-parallax src="https://vuetifyjs.com/static/doc-images/parallax/material.jpg"></v-parallax>
             </v-card>
           </v-flex>
 
-          <v-flex v-for="i in 1">
+          <v-flex :key="index" v-for="(i, index) in 1">
             <v-card class="second">
               <v-parallax src="https://vuetifyjs.com/static/doc-images/parallax/material.jpg"></v-parallax>
             </v-card>
           </v-flex>
         </v-layout>
 
-          <v-flex>
-            <v-card>
-              <v-parallax src="https://vuetifyjs.com/static/doc-images/parallax/material.jpg"></v-parallax>
-            </v-card>
-          </v-flex>
+        <v-flex>
+          <v-card>
+            <v-parallax src="https://vuetifyjs.com/static/doc-images/parallax/material.jpg"></v-parallax>
+          </v-card>
+        </v-flex>
 
-          <v-flex v-for="i in 3">
-            <v-card class="second">
-              <v-parallax src="https://vuetifyjs.com/static/doc-images/parallax/material.jpg"></v-parallax>
-            </v-card>
-          </v-flex>
+        <v-flex :key="index" v-for="(i, index) in 3">
+          <v-card class="second">
+            <v-parallax src="https://vuetifyjs.com/static/doc-images/parallax/material.jpg"></v-parallax>
+          </v-card>
+        </v-flex>
 
       </div>
     </v-container>
 
-
-</div>
+  </div>
 </template>
 
 <style>
